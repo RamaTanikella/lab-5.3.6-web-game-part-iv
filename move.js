7711d9aa4da0fb9a18a412c5ddbf5a4181ffff41
RamaTanikella <88115546+RamaTanikella@users.noreply.github.com>
2021-10-31 22:15:58 -0400

    web game part 4 solution

--- move.js
+++ move.js
@@ -5,8 +5,67 @@ function move(element) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+        function moveCharacter(){
 
+
+            if(direction === 'west'){
+                x = x-1;
+            }
+            if(direction === 'east'){
+                x = x+1;
+            }
+            if(direction === 'north'){
+                y = y+1;
+            }
+            if(direction === 'south'){
+                y = y-1;
+            }
+            
+            element.style.left = x + 'px';
+            element.style.bottom = y + 'px';
+        }
+
+        setInterval(moveCharacter, 1);
+
+        function onArrow(e){    
+            if(e.repeat){
+                return;
+            }
+            
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+
+            if(callback){
+                callback(direction)
+            }
+        }
+
+        function onArrowLetGo(e){
+            direction = null; 
+            if(callback){
+                callback(direction)
+            }
+        }
+
+        document.addEventListener('keydown', onArrow);
+        document.addEventListener('keyup', onArrowLetGo);
+    }
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
